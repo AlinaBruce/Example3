9b60a01ec91e3253cd78b715823f5ec437039ea3
AlinaBruce <85748705+AlinaBruce@users.noreply.github.com>
2021-10-29 17:22:21 +0300

    в main "New changes from GitHub!"

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello mofos!");
         System.out.println("New changes!");
+        System.out.println("New changes from GitHub!");
     }
 
 
